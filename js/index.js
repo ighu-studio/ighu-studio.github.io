28eb7dc80a9e6aba018adf186538c2820be1a31c
andezitgq <sanerhp.mail@gmail.com>
2022-07-02 02:25:24 +0300

    bazo

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-function typeWriter(txt, speed, i = 0) {
-	if (i < txt.length) {
+var txt = "";
+var rewrite = true;
+var speed = 200;
+var i = 0;
+
+function typeWriter() {
+	if (i < txt.length && rewrite == true) {
 		$('.typewriter').append(txt.charAt(i));
 		i++;
-		setTimeout(typeWriter(txt, 50, i), speed);
+		setTimeout(typeWriter, speed);
+	} else {
+		//$('.typewriter').html(tg.translate("MUZIKO", undefined, true).then(translation => {}));
 	}
 }
 
@@ -11,28 +18,26 @@ const tg = Tolgee.use(IcuFormatter).init({
   //apiUrl: 'https://app.tolgee.io',
   //apiKey: ':P',
 
-  defaultLanguage: 'eo',
+  defaultLanguage: 'ru',
   inputPrefix: '{{',
   inputSuffix: '}}',
   watch: false,
 });
 
 tg.run().then(() => {
-	var txt = tg.instant("BELETRO", undefined, true);
-	typeWriter(txt, 100);
+	document.getElementsByTagName("html")[0].style.visibility = "visible";
+	txt = tg.instant("_IGHU", undefined, true);
+	typeWriter();
+});
+
+tg.onLangLoaded.subscribe(() => {
+	if(i > 0){
+		rewrite = false;
+		$('.typewriter').html(tg.instant("_IGHU", undefined, true));
+	}
 });
 
 $('.banner').load("template/banner.txt");
-//$('head').load("template/head.html");
-//$('header').load("template/header.html");
-$('footer').load("template/footer.html");
-$('document').ready(function(){
-	$('header').ready(function(){
-		$('header').ready(function(){
-			document.getElementsByTagName("html")[0].style.visibility = "visible";
-		});
-	});
-});
 $('.lang').click(function() {
 	tg.lang = $(this).attr("value");
 });
