ff6ce99a5938d6a57089914d08e9e470b7648923
andezitgq <sanerhp.mail@gmail.com>
2022-08-19 12:49:51 +0300

    cool

--- js/index.js
+++ js/index.js
@@ -1,41 +1,66 @@
-var txt = "";
-var rewrite = true;
-var speed = 30;
-var i = 0;
+function file_get_contents( url ) {	// Reads entire file into a string
+	// 
+	// +   original by: Legaev Andrey
+	// %		note 1: This function uses XmlHttpRequest and cannot retrieve resource from different domain.
 
-function typeWriter() {
-	if (i < txt.length && rewrite == true) {
-		$('.typewriter').append(txt.charAt(i));
-		i++;
-		setTimeout(typeWriter, speed);
+	var req = null;
+	try { req = new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {
+		try { req = new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {
+			try { req = new XMLHttpRequest(); } catch(e) {}
+		}
 	}
+	if (req == null) throw new Error('XMLHttpRequest not supported');
+
+	req.open("GET", url, false);
+	req.send(null);
+
+	return req.responseText;
 }
 
 const { Tolgee, IcuFormatter } = window['@tolgee/core'];
 const tg = Tolgee.use(IcuFormatter).init({
-  //apiUrl: 'https://app.tolgee.io',
-  //apiKey: ':P',
-
-  defaultLanguage: 'ru',
-  inputPrefix: '{{',
-  inputSuffix: '}}',
-  watch: false,
+	defaultLanguage: 'eo',
+	inputPrefix: '{{',
+	inputSuffix: '}}',
+	watch: false,
 });
+
+var loaded_posts = 4;
+var posts = JSON.parse(file_get_contents("/posts/posts.json"));
+for(var i = 1; i <= Math.min(loaded_posts, Object.keys(posts).length); i++){
+	$('.post-block').append(
+	'<div class="container-md col-12 col-lg-6 col-sm-12">\
+		<div class="post m-3 bg-light rounded p-4">'
+		+
+		marked.parse(file_get_contents("/posts/" + tg.lang + "/" + posts[i] + ".md"))
+		+
+		'</div>\
+	</div>');
+}
+
+function montriplu() {
+	loaded_posts += 4;
+	for(var i = loaded_posts - 3; i <= Math.min(loaded_posts, Object.keys(posts).length); i++){
+	$('.post-block').append(
+	'<div class="container-md col-12 col-lg-6 col-sm-12">\
+		<div class="post m-3 bg-light rounded p-4">'
+		+
+		marked.parse(file_get_contents("/posts/" + tg.lang + "/" + posts[i] + ".md"))
+		+
+		'</div>\
+	</div>');
+}
+}
 
 tg.run().then(() => {
+	//$('.post').html();
 	document.getElementsByTagName("html")[0].style.visibility = "visible";
-	txt = tg.instant("_IGHU", undefined, true);
-	typeWriter();
 });
 
-tg.onLangLoaded.subscribe(() => {
-	if(i > 0){
-		rewrite = false;
-		$('.typewriter').html(tg.instant("_IGHU", undefined, true));
-	}
+tg.onLangChange.subscribe(() => {
+	//$('.post').html(marked.parse(file_get_contents("/posts/" + tg.lang + "/20220819.md")));
 });
 
-$('.banner').load("template/banner.txt");
 $('.lang').click(function() {
 	tg.lang = $(this).attr("value");
 });
